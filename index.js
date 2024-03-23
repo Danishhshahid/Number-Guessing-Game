8d71ef9d18d96ce2182407211f758e9b1b90ab1e
Danish Shahid <Danishhshahid@gmail.com>
2024-03-24 02:00:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "randomNumber",
+        type: "number",
+        message: "enter the guessed number",
+    },
+]);
+if (answers.randomNumber === randomNumber) {
+    console.log("Conrats You guessd th right number");
+}
+else {
+    console.log("try again");
+}
